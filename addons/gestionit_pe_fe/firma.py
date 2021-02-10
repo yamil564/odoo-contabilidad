8cda3610f8ba29574179c63631b318ee466ef161
Victor Daniel <danielvdmlfiis@gmail.com>
2021-02-10 18:16:49 -0500

    [ADD] sendbill xml

--- addons/gestionit_pe_fe/firma.py
+++ addons/gestionit_pe_fe/firma.py
@@ -8,6 +8,30 @@ import io
 import base64
 from xml.dom import minidom
 import os
+from . import sunat_response_handle, xml_validation
+
+
+urls = [
+    "https://e-beta.sunat.gob.pe/ol-ti-itcpfegem-beta/billService",
+    "https://www.sunat.gob.pe/ol-ti-itcpgem-sqa/billService",
+    "https://e-factura.sunat.gob.pe/ol-ti-itcpfegem/billService",
+    "https://www.sunat.gob.pe/ol-it-wsconscpegem/billConsultService",
+]
+
+# Pruebas
+urls_test = [
+    "https://e-beta.sunat.gob.pe/ol-ti-itcpfegem-beta/billService",  # Fact
+    "https://e-beta.sunat.gob.pe/ol-ti-itemision-guia-gem-beta/billService",  # Guia
+    "https://e-beta.sunat.gob.pe/ol-ti-itemision-otroscpe-gem-beta/billService",  # REte
+]
+
+# Produccion
+urls_production = [
+    "https://e-factura.sunat.gob.pe/ol-ti-itcpfegem/billService",  # Fact
+    "https://e-guiaremision.sunat.gob.pe/ol-ti-itemision-guia-gem/billService",  # Guia
+    "https://e-factura.sunat.gob.pe/ol-ti-itemision-otroscpe-gem/billService",  # REte
+]
+
 
 def firmar(document, signer, key, cert):
     document.signature = Signature.Signature(
@@ -79,3 +103,46 @@ def generate_envio_resumen_xml(username, password, file_name, doc_zip):
     body = Envelope.BodyResumen(file_name, doc_zip)
     envelope = Envelope.Envelope(header, body)
     return envelope.get_document().toxml()
+
+
+def send_xml_sunat(prev_sign, data, user):
+    tipo_envio = data.get("tipoEnvio", 0)
+
+    if tipo_envio in [1, 3]:
+        url = urls[tipo_envio]
+    elif tipo_envio == 0:
+        if prev_sign['document_type'] in ["09"]:
+            url = urls_test[1]
+        else:
+            url = urls_test[0]
+    elif tipo_envio == 2:
+        if prev_sign['document_type'] in ["09"]:
+            url = urls_production[1]
+        else:
+            url = urls_production[0]
+    elif tipo_envio == 4:
+        url = url_check_xml
+    else:
+        raise Exception("bad tipoEnvio")
+    headers = {"Content-Type": "application/xml"}
+
+    resp = requests.post(
+        url,
+        data=prev_sign['final_xml'],
+        headers=headers,
+        timeout=20)
+    response_xml = resp.text
+
+    document_type = prev_sign['document_type']
+    if document_type in ['01', '03', '07', '08', '09']:
+        resp = sunat_response_handle.get_response(response_xml)
+    elif document_type in ['RA', 'RC']:
+        resp = sunat_response_handle.get_response_ticket(response_xml)
+        # Send queue
+    else:
+        resp = {
+            "success": False,
+            "message": "Invalid document type."
+        }
+    resp['response_xml'] = response_xml
+    return resp
