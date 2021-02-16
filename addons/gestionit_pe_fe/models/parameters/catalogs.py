f31fe91fb94779539916de1b94345367ea4581e8
Christian <christian.bravo@knd.pe>
2021-02-16 10:13:36 -0500

    Cambios sale_order

---
+++ addons/gestionit_pe_fe/models/parameters/catalogs.py
@@ -0,0 +1,12 @@
+# TIPO DE COMPROBANTE
+# TIPO DE DOCUMENTO DE IDENTIDAD
+# ESTADOS DE ENVÍO
+# TIPO DE PRECIO DE VENTA UNITARIO
+#
+tdi = [
+    ("0", "DOC.TRIB.NO.DOM.SIN.RUC"),
+    ("1", "DNI"),
+    ("4", "Carnet de extranjería"),
+    ("6", "RUC"),
+    ("7", "Pasaporte"),
+    ("A", "Cédula diplomática")]
