a12e4744483d68ceff2f214e6fba0ea2bcfa0ffa
Victor Daniel <danielvdmlfiis@gmail.com>
2021-06-23 18:46:58 -0500

    [FIX] emision de comprobantes desde venta para servicios

--- addons/gestionit_pe_fe/models/sale_order/sale_order.py
+++ addons/gestionit_pe_fe/models/sale_order/sale_order.py
@@ -64,53 +64,20 @@ class SaleOrder(models.Model):
     # Create invoice
 
     def _prepare_invoice(self):
-        """
-        Prepare the dict of values to create the new invoice for a sales order. This method may be
-        overridden to implement custom invoice generation (making sure to call super() to establish
-        a clean extension chain).
-        """
-        self.ensure_one()
-        # ensure a correct context for the _get_default_journal method and company-dependent fields
-        self = self.with_context(
-            default_company_id=self.company_id.id, force_company=self.company_id.id)
-        journal = self.env['account.move'].with_context(
-            default_type='out_invoice')._get_default_journal()
-        if not journal:
-            raise UserError(_('Please define an accounting sales journal for the company %s (%s).') % (
-                self.company_id.name, self.company_id.id))
+        res = super(SaleOrder, self)._prepare_invoice()
+        res["invoice_type_code"] = self.tipo_documento
+        if len(self.env.user.warehouse_ids)>0:
+            res["warehouse_id"] = self.env.user.warehouse_ids[0].id
+            journals = self.env.user.warehouse_ids.journal_ids.filtered(lambda r:r.invoice_type_code_id ==self.tipo_documento and r.type =="sale")
+            if len(journals) > 0:
+                res["journal_id"] = journals[0].id
+            else:
+                raise UserError("Debe configurar Diarios disponibles en sus almacénes. Contacte con su administrador.")
+        else:
+            raise UserError("Su usuario no tiene almacenes disponibles. Contacte con su administrador.")
 
-        warehouse_id = self.picking_ids[0].picking_type_id.warehouse_id
+        return res
 
-        for whj in warehouse_id.journal_ids:
-            if whj.invoice_type_code_id == self.tipo_documento and whj.type == "sale":
-                journal = whj
-                break
-
-        invoice_vals = {
-            'ref': self.client_order_ref or '',
-            'type': 'out_invoice',
-            'narration': self.note,
-            'currency_id': self.pricelist_id.currency_id.id,
-            'campaign_id': self.campaign_id.id,
-            'medium_id': self.medium_id.id,
-            'source_id': self.source_id.id,
-            'invoice_user_id': self.user_id and self.user_id.id,
-            'team_id': self.team_id.id,
-            'partner_id': self.partner_invoice_id.id,
-            'partner_shipping_id': self.partner_shipping_id.id,
-            'invoice_partner_bank_id': self.company_id.partner_id.bank_ids[:1].id,
-            'fiscal_position_id': self.fiscal_position_id.id or self.partner_invoice_id.property_account_position_id.id,
-            'journal_id': journal.id,  # company comes from the journal
-            'invoice_origin': self.name,
-            'invoice_payment_term_id': self.payment_term_id.id,
-            'invoice_payment_ref': self.reference,
-            'transaction_ids': [(6, 0, self.transaction_ids.ids)],
-            'invoice_line_ids': [],
-            'company_id': self.company_id.id,
-            'invoice_type_code': self.tipo_documento,
-            'warehouse_id': warehouse_id,
-        }
-        return invoice_vals
 
     total_venta_gravado = fields.Monetary(
         string="Gravado",
