072408ba47a0397182094a64bc9e312e1dd779a4
Victor Daniel <danielvdmlfiis@gmail.com>
2021-06-26 21:42:37 -0500

    [FIX] portal precio total_venta_gravado

--- addons/gestionit_pe_fe_consulta_web/__manifest__.py
+++ addons/gestionit_pe_fe_consulta_web/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "GIT - Plataforma de Consulta WEB de comprobantes electrónicos emitidos",
     "author": "Gestión IT",
     "description": "Front-end para consulta de comprobantes",
-    "depends": ['base','portal','account'],
+    "depends": ['base','portal','sale','account'],
     "category": "invoicing",
     "data": [
         'views/views.xml',
