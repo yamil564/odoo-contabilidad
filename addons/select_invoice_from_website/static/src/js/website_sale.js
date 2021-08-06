c974704b5c87df779dd1cbcc26e3af7e469eb68b
Victor Daniel <danielvdmlfiis@gmail.com>
2021-08-06 18:36:57 -0500

    [ADD] Grupo para restriccion para el cambio de precio

--- addons/select_invoice_from_website/static/src/js/website_sale.js
+++ addons/select_invoice_from_website/static/src/js/website_sale.js
@@ -49,17 +49,20 @@ odoo.define("select_invoice_from_website.website_sale",function(require){
         },
         click_l10n_latam_identification_type_id:function(ev){
             var itc = $(ev.currentTarget).data("itc")
+            $("input[type='radio'][name='invoice_type_code']:checked").attr("checked",false)
             if(itc=="6"){
                 $(".div_company label").removeClass("label-optional")
                 $(".div_country label").removeClass("label-optional")
                 $(".div_state label").removeClass("label-optional")
                 $(".div_street label").removeClass("label-optional")
+                $("input[value='01']").attr("checked",true)
                 
             }else{
                 $(".div_company label").addClass("label-optional")
                 $(".div_country label").addClass("label-optional")
                 $(".div_street label").addClass("label-optional")
                 $(".div_state label").addClass("label-optional")
+                $("input[value='03']").attr("checked",true)
             }
 
             if(itc == "0"){
