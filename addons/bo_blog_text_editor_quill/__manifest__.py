80974b12fe188ee649278cd183a3cdff77040813
Daniel M <danielvdmlfiis@gmail.com>
2022-04-22 01:26:36 -0500

    [NEW] modulo de whatsapp para website, boton flotante

--- addons/bo_blog_text_editor_quill/__manifest__.py
+++
@@ -1,5 +0,0 @@
-{
-    "name":"Quill Editor de Texto",
-    "depends":["base","website","website_sale","website_blog"],
-    "data":["assets.xml","templates.xml","views.xml"],
-}
--- addons/bo_blog_text_editor_quill/readme.md
+++
@@ -1,3 +0,0 @@
-https://github.com/NoelOConnell/quill-image-uploader
-https://github.com/benwinding/quill-html-edit-button
-https://github.com/kensnyder/quill-image-resize-module
