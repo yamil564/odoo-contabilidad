2fdc1d3d8a1474d613cb59091e7093cebbc9b57c
FrancoNajarro2008 <franco.najarro.m@uni.pe>
2022-05-11 12:06:18 -0500

    Se corrige dependencias. Se modifica el core de pagos/cobros para que se pueda provisionar a cualquier cuenta

--- addons-contabilidad/bo_pe_contabilidad_documents/models/res_config_settings.py
+++ addons-contabilidad/bo_pe_contabilidad_documents/models/res_config_settings.py
@@ -7,11 +7,6 @@ _logger = logging.getLogger(__name__)
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
-    property_account_receivable_fees_id=fields.Many2one('account.account',
-        string="Cuenta Recibo Honorarios a Cobrar",
-        related='company_id.property_account_receivable_fees_id',readonly=False,
-        domain="[('internal_type', '=', 'receivable'), ('deprecated', '=', False)]",
-        )
 
     property_account_payable_fees_id=fields.Many2one('account.account',
         string="Cuenta Recibo Honorarios a Pagar",
@@ -39,14 +34,8 @@ class ResConfigSettings(models.TransientModel):
         domain="[('internal_type', '=', 'payable'), ('deprecated', '=', False)]",
         )
 
-    group_account_fees_ME = fields.Boolean("Cuentas de Recibos por Honorario por Cobrar y por Pagar en Divisa",  implied_group='base.group_multi_currency', default=False)#,
+    group_account_fees_ME = fields.Boolean("Cuentas de Recibos por Honorario por Pagar en Divisa",  implied_group='base.group_multi_currency', default=False)#,
 
-    property_account_receivable_me_fees_id=fields.Many2one('account.account',
-        string="Cuenta Recibo Honorarios a Cobrar ME",
-        related='company_id.property_account_receivable_me_fees_id',
-        implied_group='base.group_multi_currency',readonly=False,
-        domain="[('internal_type', '=', 'receivable'), ('deprecated', '=', False)]",
-        )
 
     property_account_payable_me_fees_id=fields.Many2one('account.account',
         string="Cuenta Recibo Honorarios a Pagar ME",
@@ -70,26 +59,20 @@ class ResConfigSettings(models.TransientModel):
 
     def sync_account_fees_partners(self):
     	query_update="""
-    		update res_partner set property_account_receivable_fees_id=%s,
-    			property_account_payable_fees_id=%s 
+    		update res_partner set property_account_payable_fees_id=%s 
     		where 
     			active=true and 
-    			massive_update_account_fees=true """%(
-    				self.property_account_receivable_fees_id.id,
-    				self.property_account_payable_fees_id.id)
+    			massive_update_account_fees=true """%(self.property_account_payable_fees_id.id)
 
     	self.env.cr.execute(query_update)
 
 
     def sync_account_me_fees_partners(self):
     	query_update="""
-    		update res_partner set property_account_receivable_me_fees_id=%s,
-    			property_account_payable_me_fees_id=%s 
+    		update res_partner set property_account_payable_me_fees_id=%s 
     		where 
     			active=true and 
-    			massive_update_account_fees_me=true """%(
-    				self.property_account_receivable_me_fees_id.id,
-    				self.property_account_payable_me_fees_id.id)
+    			massive_update_account_fees_me=true """%(self.property_account_payable_me_fees_id.id)
 
     	self.env.cr.execute(query_update)
 
