eb03cdc08f9ba1983b137b319272a2d2902bb934
Daniel M <danielvdmlfiis@gmail.com>
2022-08-30 19:08:05 -0500

    [UPD] delivery perú

--- addons-website/bo_delivery_pe/__manifest__.py
+++ addons-website/bo_delivery_pe/__manifest__.py
@@ -2,6 +2,6 @@
     "name":"Delivery Perú",
     "depends":["base","delivery","website_sale","website_sale_delivery","gestionit_pe_ubicaciones"],
     "data":[
-        
+        "views/views.xml"
     ]
 }
